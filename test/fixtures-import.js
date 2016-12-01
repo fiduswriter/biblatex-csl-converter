7a32b9a56499e7ebfd3ac5d70b6b5d763fd2f5e3
retorquere <emiliano.heyns@iris-advies.com>
2016-12-01 17:12:51 +0100

    jabref groups parser for #40 -- doesn't work yet because the separator chars are replaced before parse

--- test/fixtures-import.js
+++ test/fixtures-import.js
@@ -5,6 +5,12 @@ import {BibLatexParser} from "../src/import/biblatex"
 const fs = require('fs');
 const path = require('path');
 
+const clean = state => {
+  for (let prop of ['errors', 'warnings', 'groups']) {
+    if (!state[prop] || state[prop].length == 0) { delete state[prop] }
+  }
+}
+
 const verify = bibfile => {
   let input = fs.readFileSync(bibfile, 'utf8');
   let parser = new BibLatexParser(input, {rawFields: true, processUnexpected: true, processUnknown: true});
@@ -13,13 +19,11 @@ const verify = bibfile => {
   // this must be called before requesting warnings or errors
   let references = parser.output;
   let found = { references, errors: parser.errors, warnings: parser.warnings };
-  if (!found.errors || found.errors.length == 0)            { delete found.errors; }
-  if (!found.warnings || found.warnings.length == 0)        { delete found.warnings; }
+  clean(found)
 
   let expected = path.join(path.dirname(bibfile), name + '.json');
   expected = JSON.parse(fs.readFileSync(expected, 'utf8'));
-  if (!expected.errors || expected.errors.length == 0)      { delete expected.errors; }
-  if (!expected.warnings || expected.warnings.length == 0)  { delete expected.warnings; }
+  clean(expected)
 
   test(name, t => t.deepEqual(expected, found));
 }
