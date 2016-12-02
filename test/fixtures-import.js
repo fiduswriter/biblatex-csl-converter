350060add07638cc653fa2a0e693c5b3b2f3ca54
Johannes Wilm <mail@johanneswilm.org>
2016-12-02 11:28:15 +0100

    remove semi-colons

--- test/fixtures-import.js
+++ test/fixtures-import.js
@@ -2,8 +2,8 @@ import test from "ava"
 
 import {BibLatexParser} from "../src/import/biblatex"
 
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const clean = state => {
   for (let prop of ['errors', 'warnings', 'groups']) {
@@ -12,26 +12,26 @@ const clean = state => {
 }
 
 const verify = bibfile => {
-  let input = fs.readFileSync(bibfile, 'utf8');
-  let parser = new BibLatexParser(input, {rawFields: true, processUnexpected: true, processUnknown: true});
-  let name = path.basename(bibfile, path.extname(bibfile));
+  let input = fs.readFileSync(bibfile, 'utf8')
+  let parser = new BibLatexParser(input, {rawFields: true, processUnexpected: true, processUnknown: true})
+  let name = path.basename(bibfile, path.extname(bibfile))
 
   // this must be called before requesting warnings or errors
-  let references = parser.output;
-  let found = { references, groups: parser.groups, errors: parser.errors, warnings: parser.warnings };
+  let references = parser.output
+  let found = { references, groups: parser.groups, errors: parser.errors, warnings: parser.warnings }
   clean(found)
 
-  let expected = path.join(path.dirname(bibfile), name + '.json');
-  expected = JSON.parse(fs.readFileSync(expected, 'utf8'));
+  let expected = path.join(path.dirname(bibfile), name + '.json')
+  expected = JSON.parse(fs.readFileSync(expected, 'utf8'))
   clean(expected)
 
-  test(name, t => t.deepEqual(expected, found));
+  test(name, t => t.deepEqual(expected, found))
 }
 
-const fixtures = path.join(__dirname, 'fixtures/import');
-const bibfiles = fs.readdirSync(fixtures);
+const fixtures = path.join(__dirname, 'fixtures/import')
+const bibfiles = fs.readdirSync(fixtures)
 for (let i in bibfiles) {
-  let fixture = path.join(fixtures, bibfiles[i]);
-  if (path.extname(fixture) != '.bib') { continue; }
+  let fixture = path.join(fixtures, bibfiles[i])
+  if (path.extname(fixture) != '.bib') { continue }
   verify(fixture)
 }
