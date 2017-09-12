c07af0009fde4c1d425afe2406a6f844cc9a8d4d
Johannes Wilm <mail@johanneswilm.org>
2017-09-12 18:40:35 +0200

    split out group parser, relates to #79

--- src/import/biblatex.js
+++ src/import/biblatex.js
@@ -2,6 +2,7 @@ import {BibFieldTypes, BibTypes} from "../const"
 import {TeXSpecialChars, BiblatexAliasTypes, BiblatexFieldAliasTypes, BiblatexAliasOptions} from "./const"
 import {BibLatexNameParser} from "./name-parser"
 import {BibLatexLiteralParser} from "./literal-parser"
+import {GroupParser} from "./group-parser"
 import {splitTeXString} from "./tools"
 import {edtfCheck} from "../edtf"
 
@@ -677,8 +678,8 @@ export class BibLatexParser {
                 this.string()
             } else if (d == "@preamble") {
                 this.preamble()
-            } else if (d == "@comment") {
-                this.parseGroups()
+            } else if (d == "@comment" || d == "@Comment") {
+                this.parseComment()
             } else {
                 this.createNewEntry()
             }
@@ -686,99 +687,31 @@ export class BibLatexParser {
         }
     }
 
-    parseGroups() {
-      const prefix = 'jabref-meta: groupstree:'
-      let pos = this.input.indexOf(prefix, this.pos)
-      if (pos < 0) { return }
-      this.pos = pos + prefix.length
-
-      /*  The JabRef Groups format is... interesting. To parse it, you must:
-          1. Unwrap the lines (just remove the newlines)
-          2. Split the lines on ';' (but not on '\;')
-          3. Each line is a group which is formatted as follows:
-             <level> <type>:<name>\;<intersect>\;<citekey1>\;<citekey2>\;....
-
-          Each level can interact with the level it is nested under; either no interaction (intersect = 0), intersection
-          (intersect = 1) or union (intersect = 2).
-
-          There are several group types: root-level (all references are implicitly available on the root level),
-          ExplicitGroup (the citation keys are listed in the group line) or query-type groups. I have only implemented
-          explicit groups.
-      */
-
-      // skip any whitespace after the identifying string */
-      while ((this.input.length > this.pos) && ('\r\n '.indexOf(this.input[this.pos]) >= 0)) { this.pos++ }
-
-      let start = this.pos
-      let braces = 1
-      while (this.input.length > this.pos && braces > 0) {
-        switch (this.input[this.pos]) {
-          case '{':
-            braces += 1
-            break
-          case '}':
-            braces -= 1
-        }
-        this.pos++
-      }
-
-      // no ending brace found
-      if (braces !== 0) { return }
-
-      // leave the ending brace for the main parser to pick up
-      this.pos--
-
-      // simplify parsing by taking the whole comment, throw away newlines, replace the escaped separators with tabs, and
-      // then split on the remaining non-escaped separators
-      // I use \u2004 to protect \; and \u2005 to protect \\\; (the escaped version of ';') when splitting lines at ;
-      let lines = this.input.substring(start, this.pos).replace(/[\r\n]/g, '').replace(/\\\\\\;/g, '\u2005').replace(/\\;/g, '\u2004').split(';')
-      lines = lines.map(line => {
-          return line.replace(/\u2005/g,';')
-      })
-      let levels = { '0': { references: [], groups: [] } }
-      for (let line of lines) {
-        if (line === '') { continue }
-        let match = line.match(/^([0-9])\s+([^:]+):(.*)/)
-        if (!match) { return }
-        let level = parseInt(match[1])
-        let type = match[2]
-        let references = match[3]
-        references = references ? references.split('\u2004').filter(key => key) : []
-        let name = references.shift()
-        let intersection = references.shift() // 0 = independent, 1 = intersection, 2 = union
-
-        // ignore root level, has no refs anyway in the comment
-        if (level === 0) { continue }
-
-        // remember this group as the current `level` level, so that any following `level + 1` levels can find it
-        levels[level] = { name, groups: [], references }
-        // and add it to its parent
-        levels[level - 1].groups.push(levels[level])
-
-        // treat all groups as explicit
-        if (type != 'ExplicitGroup') {
-            this.warnings.push({
-                type: 'unsupported_jabref_group',
-                group_type: type
-            })
+    parseComment() {
+        let start = this.pos
+        let braces = 1
+        while (this.input.length > this.pos && braces > 0) {
+          switch (this.input[this.pos]) {
+            case '{':
+              braces += 1
+              break
+            case '}':
+              braces -= 1
+          }
+          this.pos++
         }
 
-        switch (intersection) {
-          case '0':
-            // do nothing more
-            break
-          case '1':
-            // intersect with parent. Hardly ever used.
-            levels[level].references = levels[level].references.filter(key => levels[level - 1].references.includes(key))
-            break
-          case '2':
-            // union with parent
-            levels[level].references = [...new Set([...levels[level].references, ...levels[level - 1].references])]
-            break
-        }
-      }
+        // no ending brace found
+        if (braces !== 0) { return }
 
-      this.groups = levels['0'].groups
+        // leave the ending brace for the main parser to pick up
+        this.pos--
+        let comment = this.input.substring(start, this.pos)
+        let groupParser = new GroupParser(comment)
+        groupParser.init()
+        if (groupParser.groups) {
+            this.groups = groupParser.groups
+        }
     }
 
     createBibDB() {
---
+++ src/import/group-parser.js
@@ -0,0 +1,84 @@
+export class GroupParser {
+    constructor(input) {
+      this.input = input
+      this.pos = 0
+      this.groups = false
+      this.warnings = []
+    }
+
+    init() {
+        const prefix = 'jabref-meta: groupstree:'
+        let pos = this.input.indexOf(prefix, this.pos)
+        if (pos < 0) {
+            return
+        }
+        this.pos = pos + prefix.length
+
+      /*  The JabRef Groups format is... interesting. To parse it, you must:
+          1. Unwrap the lines (just remove the newlines)
+          2. Split the lines on ';' (but not on '\;')
+          3. Each line is a group which is formatted as follows:
+             <level> <type>:<name>\;<intersect>\;<citekey1>\;<citekey2>\;....
+
+          Each level can interact with the level it is nested under; either no interaction (intersect = 0), intersection
+          (intersect = 1) or union (intersect = 2).
+
+          There are several group types: root-level (all references are implicitly available on the root level),
+          ExplicitGroup (the citation keys are listed in the group line) or query-type groups. I have only implemented
+          explicit groups.
+      */
+        // skip any whitespace after the identifying string */
+        while (
+            (this.input.length > this.pos) &&
+            ('\r\n '.indexOf(this.input[this.pos]) >= 0)
+        ) { this.pos++ }
+        // simplify parsing by taking the whole comment, throw away newlines, replace the escaped separators with tabs, and
+        // then split on the remaining non-escaped separators
+        // I use \u2004 to protect \; and \u2005 to protect \\\; (the escaped version of ';') when splitting lines at ;
+        let lines = this.input.substring(this.pos).replace(/[\r\n]/g, '').replace(/\\\\\\;/g, '\u2005').replace(/\\;/g, '\u2004').split(';')
+        lines = lines.map(line => line.replace(/\u2005/g,';'))
+        let levels = { '0': { references: [], groups: [] } }
+        for (let line of lines) {
+            if (line === '') { continue }
+            let match = line.match(/^([0-9])\s+([^:]+):(.*)/)
+            if (!match) { return }
+            let level = parseInt(match[1])
+            let type = match[2]
+            let references = match[3]
+            references = references ? references.split('\u2004').filter(key => key) : []
+            let name = references.shift()
+            let intersection = references.shift() // 0 = independent, 1 = intersection, 2 = union
+
+            // ignore root level, has no refs anyway in the comment
+            if (level === 0) { continue }
+
+            // remember this group as the current `level` level, so that any following `level + 1` levels can find it
+            levels[level] = { name, groups: [], references }
+            // and add it to its parent
+            levels[level - 1].groups.push(levels[level])
+
+            // treat all groups as explicit
+            if (type != 'ExplicitGroup') {
+                this.warnings.push({
+                    type: 'unsupported_jabref_group',
+                    group_type: type
+                })
+            }
+
+            switch (intersection) {
+                case '0':
+                // do nothing more
+                break
+                case '1':
+                // intersect with parent. Hardly ever used.
+                levels[level].references = levels[level].references.filter(key => levels[level - 1].references.includes(key))
+                break
+                case '2':
+                // union with parent
+                levels[level].references = [...new Set([...levels[level].references, ...levels[level - 1].references])]
+                break
+            }
+        }
+        this.groups = levels['0'].groups
+    }
+}
