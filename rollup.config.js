1e8ce03a683a340fe5f756326cd3785039628e7a
Johannes Wilm <mail@johanneswilm.org>
2019-01-26 12:36:24 +0100

    uglify-es => terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,22 @@
-module.exports = {
+import commonjs from 'rollup-plugin-commonjs'
+import globals from 'rollup-plugin-node-globals'
+import builtins from 'rollup-plugin-node-builtins'
+import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
-  plugins: [
-    require('rollup-plugin-commonjs')(),
-    require('rollup-plugin-node-globals')(),
-    require('rollup-plugin-node-builtins')(),
-    require('rollup-plugin-node-resolve')(),
-    require('rollup-plugin-json')(),
-    require('rollup-plugin-babel')({
-        runtimeHelpers: true,
-        exclude: 'node_modules/**'
-    }),
-    require('rollup-plugin-uglify-es')()
-  ]
+export default {
+    plugins: [
+        commonjs(),
+        globals(),
+        builtins(),
+        resolve(),
+        json(),
+        babel({
+            runtimeHelpers: true,
+            exclude: 'node_modules/**'
+        }),
+        terser()
+    ]
 }
