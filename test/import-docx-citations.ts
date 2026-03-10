ece49637cd17dc856f0a7ac12eaf4372226b16e3
Johannes Wilm <mail@johanneswilm.org>
2026-03-10 14:24:03 +0100

    add test for external usage of ODT/DOCX citation importer fiduswriter/fiduswriter#1351

--- test/import-docx-citations.ts
+++ test/import-docx-citations.ts
@@ -81,3 +81,212 @@ for (let filename of allFiles) {
 
     verify(xmlFile, sourcesFile)
 }
+
+// Static utility methods tests
+describe("Static utility methods", () => {
+    describe("DocxCitationsParser", () => {
+        describe("sdtCitation", () => {
+            it("detects Mendeley v3 citation SDT (check only)", () => {
+                const sdtXml = `<w:sdt>
+                    <w:sdtPr>
+                        <w:tag w:val="MENDELEY_CITATION_v3_eyJjaXRhdGlvbklE"/>
+                    </w:sdtPr>
+                </w:sdt>`
+                const result = converter.DocxCitationsParser.sdtCitation(
+                    sdtXml,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("mendeley_v3")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("detects Citavi citation SDT (check only)", () => {
+                const sdtXml = `<w:sdt>
+                    <w:sdtPr>
+                        <w:tag w:val="CitaviPlaceholder#12345"/>
+                    </w:sdtPr>
+                </w:sdt>`
+                const result = converter.DocxCitationsParser.sdtCitation(
+                    sdtXml,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("citavi")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("returns false for non-citation SDT", () => {
+                const sdtXml = `<w:sdt>
+                    <w:sdtPr>
+                        <w:tag w:val="SOME_OTHER_TAG"/>
+                    </w:sdtPr>
+                </w:sdt>`
+                const result = converter.DocxCitationsParser.sdtCitation(
+                    sdtXml,
+                    false
+                )
+                expect(result.isCitation).to.equal(false)
+                expect(result.format).to.equal(undefined)
+            })
+        })
+
+        describe("sdtBibliography", () => {
+            it("detects Mendeley bibliography SDT", () => {
+                const sdtXml = `<w:sdt>
+                    <w:sdtPr>
+                        <w:tag w:val="MENDELEY_BIBLIOGRAPHY_v3_abc"/>
+                    </w:sdtPr>
+                </w:sdt>`
+                const result = converter.DocxCitationsParser.sdtBibliography(
+                    sdtXml,
+                    false
+                )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("mendeley_v3")
+            })
+
+            it("returns false for non-bibliography SDT", () => {
+                const sdtXml = `<w:sdt>
+                    <w:sdtPr>
+                        <w:tag w:val="MENDELEY_CITATION_v3_abc"/>
+                    </w:sdtPr>
+                </w:sdt>`
+                const result = converter.DocxCitationsParser.sdtBibliography(
+                    sdtXml,
+                    false
+                )
+                expect(result.isBibliography).to.equal(false)
+            })
+        })
+
+        describe("fieldCitation", () => {
+            it("detects Zotero field citation (check only)", () => {
+                const instr = "ADDIN ZOTERO_ITEM CSL_CITATION {}"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("zotero")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("detects Mendeley legacy field citation (check only)", () => {
+                const instr = "ADDIN CSL_CITATION {}"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("mendeley_legacy")
+            })
+
+            it("detects EndNote field citation (check only)", () => {
+                const instr = "ADDIN EN.CITE <EndNote></EndNote>"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("endnote")
+            })
+
+            it("detects Word native citation (check only)", () => {
+                const instr = "CITATION Smith2020 \\l 1033"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("word_native")
+            })
+
+            it("returns false for non-citation field", () => {
+                const instr = "REF _Ref123456 \\h"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    false
+                )
+                expect(result.isCitation).to.equal(false)
+            })
+
+            it("extracts Zotero citation data", () => {
+                const instr = ` ADDIN ZOTERO_ITEM CSL_CITATION {
+                    "citationID": "test",
+                    "properties": {},
+                    "citationItems": [{
+                        "id": 1,
+                        "itemData": {
+                            "id": 1,
+                            "type": "article-journal",
+                            "title": "Test Article",
+                            "author": [{"family": "Smith", "given": "John"}],
+                            "issued": {"date-parts": [[2020]]}
+                        }
+                    }]
+                } `
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    true
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("zotero")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.be.greaterThan(0)
+                expect(result.errors!.length).to.equal(0)
+            })
+
+            it("returns empty result for non-citation field", () => {
+                const instr = "REF _Ref123456 \\h"
+                const result = converter.DocxCitationsParser.fieldCitation(
+                    instr,
+                    true
+                )
+                expect(result.isCitation).to.equal(false)
+                expect(result.format).to.equal(undefined)
+            })
+        })
+
+        describe("fieldBibliography", () => {
+            it("detects Zotero bibliography field", () => {
+                const instr = "ADDIN ZOTERO_BIBL {} CSL_BIBLIOGRAPHY"
+                const result = converter.DocxCitationsParser.fieldBibliography(
+                    instr,
+                    false
+                )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("zotero")
+            })
+
+            it("detects EndNote bibliography field", () => {
+                const instr = "ADDIN EN.REFLIST"
+                const result = converter.DocxCitationsParser.fieldBibliography(
+                    instr,
+                    false
+                )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("endnote")
+            })
+
+            it("detects Word native bibliography field", () => {
+                const instr = "BIBLIOGRAPHY \\l 1033"
+                const result = converter.DocxCitationsParser.fieldBibliography(
+                    instr,
+                    false
+                )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("word_native")
+            })
+
+            it("returns false for non-bibliography field", () => {
+                const instr = "ADDIN ZOTERO_ITEM CSL_CITATION {}"
+                const result = converter.DocxCitationsParser.fieldBibliography(
+                    instr,
+                    false
+                )
+                expect(result.isBibliography).to.equal(false)
+            })
+        })
+    })
+})
--- test/import-odt-citations.ts
+++ test/import-odt-citations.ts
@@ -68,3 +68,350 @@ for (let filename of allFiles) {
     const xmlFile = path.join(fixturesDir, filename)
     verify(xmlFile)
 }
+
+// Static utility methods tests
+describe("Static utility methods", () => {
+    describe("OdtCitationsParser", () => {
+        describe("referenceMarkCitation", () => {
+            it("detects Zotero reference mark (check only)", () => {
+                const markName =
+                    'ZOTERO_ITEM CSL_CITATION {"citationID":"test"}'
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        false
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("zotero")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("detects Mendeley legacy reference mark (check only)", () => {
+                const markName = 'CSL_CITATION {"citationID":"test"}'
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        false
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("mendeley_legacy")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("detects JabRef reference mark (check only)", () => {
+                const markName = "JABREF_Smith2020 CID_1 abc123"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        false
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("jabref")
+                expect(result.entries).to.equal(undefined)
+            })
+
+            it("returns false for non-citation mark", () => {
+                const markName = "SOME_OTHER_MARK"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        false
+                    )
+                expect(result.isCitation).to.equal(false)
+            })
+
+            it("extracts Zotero citation data", () => {
+                const markName = `ZOTERO_ITEM CSL_CITATION {
+                    "citationID": "test",
+                    "properties": {},
+                    "citationItems": [{
+                        "id": 1,
+                        "itemData": {
+                            "id": 1,
+                            "type": "article-journal",
+                            "title": "Test Article",
+                            "author": [{"family": "Smith", "given": "John"}],
+                            "issued": {"date-parts": [[2020]]}
+                        }
+                    }]
+                } RNDLQoAq6M7m4`
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        true
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("zotero")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.be.greaterThan(0)
+                expect(result.errors!.length).to.equal(0)
+            })
+
+            it("handles Zotero citation with random ID suffix", () => {
+                const markName = `ZOTERO_ITEM CSL_CITATION {
+                    "citationID": "test123",
+                    "properties": {},
+                    "citationItems": [{
+                        "id": 2,
+                        "itemData": {
+                            "id": 2,
+                            "type": "book",
+                            "title": "Test Book",
+                            "author": [{"family": "Jones", "given": "Jane"}],
+                            "issued": {"date-parts": [[2021]]}
+                        }
+                    }]
+                } RNDjURflxg9F1`
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        true
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("zotero")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.equal(1)
+                const entry = Object.values(result.entries!)[0]
+                expect(entry.fields.title).to.deep.equal([
+                    { type: "text", text: "Test Book" },
+                ])
+                expect(result.errors!.length).to.equal(0)
+            })
+
+            it("handles various random ID formats", () => {
+                // Test with different random ID patterns (10 alphanumeric chars)
+                const testCases = [
+                    " RNDabcdef1234",
+                    " RNDABCDEF1234",
+                    " RNDaBc12XyZ89",
+                    " RND0123456789",
+                ]
+
+                for (const suffix of testCases) {
+                    const markName = `ZOTERO_ITEM CSL_CITATION {"citationID":"test","properties":{},"citationItems":[{"id":1,"itemData":{"id":1,"type":"article-journal","title":"Test","author":[{"family":"Smith","given":"John"}],"issued":{"date-parts":[[2020]]}}}]}${suffix}`
+                    const result =
+                        converter.OdtCitationsParser.referenceMarkCitation(
+                            markName,
+                            true
+                        )
+                    expect(result.isCitation).to.equal(true)
+                    expect(result.format).to.equal("zotero")
+                    expect(result.entries).to.not.equal(undefined)
+                    expect(
+                        Object.keys(result.entries!).length
+                    ).to.be.greaterThan(0)
+                }
+            })
+
+            it("extracts JabRef citation data", () => {
+                const markName = "JABREF_Smith2020 CID_1 abc123"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        true
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("jabref")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.be.greaterThan(0)
+                // JabRef marks create stub entries
+                const entry = Object.values(result.entries!)[0]
+                expect(entry.bib_type).to.equal("misc")
+            })
+
+            it("returns empty result for non-citation mark", () => {
+                const markName = "SOME_OTHER_MARK"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkCitation(
+                        markName,
+                        true
+                    )
+                expect(result.isCitation).to.equal(false)
+                expect(result.format).to.equal(undefined)
+            })
+        })
+
+        describe("referenceMarkBibliography", () => {
+            it("detects Zotero bibliography mark", () => {
+                const markName = "ZOTERO_BIBL CSL_BIBLIOGRAPHY RNDEk1MPyJ9IL"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkBibliography(
+                        markName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("zotero")
+            })
+
+            it("detects Zotero bibliography mark with JSON and random ID", () => {
+                const markName =
+                    'ZOTERO_BIBL {"uncited":[],"custom":[]} CSL_BIBLIOGRAPHY RNDjURflxg9F1'
+                const result =
+                    converter.OdtCitationsParser.referenceMarkBibliography(
+                        markName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("zotero")
+            })
+
+            it("detects Mendeley bibliography mark", () => {
+                const markName = "CSL_BIBLIOGRAPHY"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkBibliography(
+                        markName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("mendeley_legacy")
+            })
+
+            it("returns false for non-bibliography mark", () => {
+                const markName = "ZOTERO_ITEM CSL_CITATION {} RNDLQoAq6M7m4"
+                const result =
+                    converter.OdtCitationsParser.referenceMarkBibliography(
+                        markName
+                    )
+                expect(result.isBibliography).to.equal(false)
+            })
+        })
+
+        describe("bibliographyMarkCitation", () => {
+            it("detects LibreOffice native bibliography mark", () => {
+                const bibMarkXml =
+                    '<text:bibliography-mark text:identifier="Smith2020"/>'
+                const result =
+                    converter.OdtCitationsParser.bibliographyMarkCitation(
+                        bibMarkXml
+                    )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("libreoffice_native")
+            })
+
+            it("returns false for non-bibliography-mark element", () => {
+                const xml = "<text:span>Some text</text:span>"
+                const result =
+                    converter.OdtCitationsParser.bibliographyMarkCitation(xml)
+                expect(result.isCitation).to.equal(false)
+            })
+        })
+
+        describe("sectionBibliography", () => {
+            it("detects JabRef bibliography section (lowercase)", () => {
+                const sectionName = "JR_bib"
+                const result =
+                    converter.OdtCitationsParser.sectionBibliography(
+                        sectionName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("jabref")
+            })
+
+            it("detects JabRef bibliography section (uppercase)", () => {
+                const sectionName = "JR_BIB"
+                const result =
+                    converter.OdtCitationsParser.sectionBibliography(
+                        sectionName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("jabref")
+            })
+
+            it("detects JabRef bibliography section (mixed case)", () => {
+                const sectionName = "Jr_BiB"
+                const result =
+                    converter.OdtCitationsParser.sectionBibliography(
+                        sectionName
+                    )
+                expect(result.isBibliography).to.equal(true)
+                expect(result.format).to.equal("jabref")
+            })
+
+            it("returns false for non-bibliography section", () => {
+                const sectionName = "SOME_OTHER_SECTION"
+                const result =
+                    converter.OdtCitationsParser.sectionBibliography(
+                        sectionName
+                    )
+                expect(result.isBibliography).to.equal(false)
+                expect(result.format).to.equal(undefined)
+            })
+
+            it("returns false for similar but incorrect section name", () => {
+                const sectionName = "JR_bibliography"
+                const result =
+                    converter.OdtCitationsParser.sectionBibliography(
+                        sectionName
+                    )
+                expect(result.isBibliography).to.equal(false)
+            })
+        })
+
+        describe("endNotePlaceholder", () => {
+            it("detects EndNote placeholder pattern (check only)", () => {
+                const text =
+                    "According to {Smith, 2020 #291}, the research shows..."
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("endnote")
+            })
+
+            it("detects multiple EndNote placeholders (check only)", () => {
+                const text = "{Smith, 2020 #291; Jones, 2019 #47}"
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    false
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("endnote")
+            })
+
+            it("returns false for text without placeholders", () => {
+                const text =
+                    "Just some regular text with {braces} but no #number"
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    false
+                )
+                expect(result.isCitation).to.equal(false)
+            })
+
+            it("extracts EndNote placeholder data", () => {
+                const text = "Research shows {Smith, 2020 #291} that..."
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    true
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("endnote")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.be.greaterThan(0)
+                const entry = Object.values(result.entries!)[0]
+                expect(entry.entry_key).to.equal("EN291")
+                expect(entry.bib_type).to.equal("misc")
+            })
+
+            it("extracts multiple EndNote placeholders", () => {
+                const text = "{Smith, 2020 #291; Jones, 2019 #47}"
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    true
+                )
+                expect(result.isCitation).to.equal(true)
+                expect(result.format).to.equal("endnote")
+                expect(result.entries).to.not.equal(undefined)
+                expect(Object.keys(result.entries!).length).to.equal(2)
+            })
+
+            it("returns empty result for non-placeholder text", () => {
+                const text = "Just regular text"
+                const result = converter.OdtCitationsParser.endNotePlaceholder(
+                    text,
+                    true
+                )
+                expect(result.isCitation).to.equal(false)
+                expect(result.format).to.equal(undefined)
+            })
+        })
+    })
+})
